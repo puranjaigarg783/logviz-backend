308909d368c8b45fb2b2bbafd56815c1e7f42c01
Vishal Dani <vishal.dani123@gmail.com>
2023-11-18 16:23:26 -0800

    Initial Commit

---
+++ src/main/java/com/logviz/LogVizApplication.java
@@ -0,0 +1,13 @@
+package com.logviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogVizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogVizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logviz/LogVizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogVizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
